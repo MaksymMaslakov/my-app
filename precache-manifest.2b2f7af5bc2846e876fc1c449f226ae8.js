37689dbd68e841837c9e846bd0befd890813faa7
MaksymMaslakov <kamsam243@gmail.com>
2020-10-10 20:46:23 +0300

    Updates

---
+++ precache-manifest.2b2f7af5bc2846e876fc1c449f226ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30379ab88a29435b794054465579f127",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "360417ca4582ee7518df",
+    "url": "/my-app/static/css/main.7f85b3a4.chunk.css"
+  },
+  {
+    "revision": "0bdd47b88ecf87070ce2",
+    "url": "/my-app/static/js/2.6b78a115.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.6b78a115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360417ca4582ee7518df",
+    "url": "/my-app/static/js/main.2af3301a.chunk.js"
+  },
+  {
+    "revision": "3468c923e9fd6b35b477",
+    "url": "/my-app/static/js/runtime-main.7720b7a2.js"
+  }
+]);
